76ebf7b3953d9c71f485f5b3e5d7b345ec056d55
Morgan Langford <hofley@hotmail.com>
2019-09-05 12:26:12 -0700

    This is a commit

---
+++ task0/task0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	printf("Hello world");
+}
+
+/* MSYS commands:
+	mkdir = make directory
+	ls = list 
+	cd = change directory
+	gcc task0.c = compiles it
+	ls
+	./a.exe = runs it
+

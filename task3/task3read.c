9f51f57a5d3d15221cc8f90651a438b2c958c38a
Morgan Langford <hofley@hotmail.com>
2019-09-23 10:11:41 -0700

    all up to date

--- task3/task3read.c
+++ task3/task3read.c
@@ -2,28 +2,27 @@
 #include <stdlib.h>
 #include <math.h>
 
-float findMean(float* input, int len){
-	printf("\nMade it to findMean");
+float findMean(float* input, int len){	// this function finds the mean
 	float mean = 0;
 
 	for (int i=0; i<len; i++){
-		mean += input[i];
+		mean += input[i];			// this is actually finding the sum
 	}
 
-	mean = mean/len;
+	mean = mean/len;				// mean = sum/#values
 	printf("\nMean = %f",mean);
 
 	return mean;
 }
 
-float findStdDev(float* input, float mean, int len){
+float findStdDev(float* input, float mean, int len){	// this function finds the std dev
 	float sum;
 
 	for (int j=0; j<len; j++){
-		sum += powf((input[j] - mean),2);
+		sum += powf((input[j] - mean),2);				// sum of the squared differences
 	}
 
-	float stdDev = sqrt(sum/(len-1));
+	float stdDev = sqrt(sum/(len-1));					// equation for std dev using the mean from previous function
 	printf("Standard deviation = %f",stdDev);
 
 	return stdDev;
@@ -42,7 +41,7 @@ int main(){
 		while (fscanf(dataFile,"%f %f",&x,&z) != EOF){ // counting how many rows are in the file
 			counter++;
 		}
-		//printf("Counter = %i",counter);
+		
 		float y[counter]; 					// creating an array of length of file
 
 		rewind(dataFile); 					// start at the beginning of the file again
@@ -50,11 +49,9 @@ int main(){
 		for (int i = 0; i<counter; i++){	// put all y values into an array called y
 			fscanf(dataFile,"%f %f",&x,&y[i]);
 		}
-		printf("\nmade it here");
-		float mean = findMean(y,counter); 	// send array y and length to function findMean
-		findStdDev(y,mean,counter);		// call next function to find standard dev
-
 		
+		float mean = findMean(y,counter); 	// send array y and length to function findMean
+		findStdDev(y,mean,counter);			// call next function to find standard dev
 	}
 	else printf("\nCouldn't read file.");	// uf the file couldn't be opened...
 

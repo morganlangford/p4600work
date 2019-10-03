7c36871d9213c651f23bbd86b8825aa1400a45f0
U-PHYSICSR303\Student <Student@PhysicsR303.kwantlen.ca>
2019-10-03 11:49:32 -0700

    this isn't working

---
+++ task5/src/main.c
@@ -0,0 +1,43 @@
+  
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>	
+#include <visa.h>
+
+void main(int argc, char** argv)
+{
+	char inq[] = "*IDN?/n";
+	ViStatus status = VI_SUCCESS;
+	ViFindList resourceList;
+	ViUInt32 num_inst;
+
+	ViSession defaultRM, scopeHandle;
+	ViChar description[VI_FIND_BUFLEN];
+
+	status = viOpenDefaultRM(&defaultRM);
+
+	if(status == VI_SUCCESS)
+	{
+		status = viFindRsrc(defaultRM,"USB[0-9]::*INSTR",
+						&resourceList,&num_inst,description);
+		if(status == VI_SUCCESS)
+		{
+			status = viOpen(defaultRM,description,
+							VI_NULL,VI_NULL,&scopeHandle);
+
+			if(status == VI_SUCCESS)
+			{
+				printf("Opened %s",description);
+			}
+			else
+			{
+				printf("Failed to open %s",description);
+			}
+		}
+		else
+		{
+			printf("Couldn't find any instruments");
+		}
+	}
+	printf("Failed to open defaultRM");
+}

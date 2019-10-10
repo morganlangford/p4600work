9df1c74a048e32545f0ad63d19076e0013616af6
Morgan Langford <hofley@hotmail.com>
2019-10-10 07:40:10 -0700

    task4 working I think

--- task4/src/main.c
+++ task4/src/main.c
@@ -9,35 +9,51 @@
 /* This is the main file for controlling the function generator and the oscilloscope
 	and processing the data from the oscilloscope */
 
-int main(){
-	/* This is for testing purposes */
+void main(){
+	/* 	Basically this main was just for testing to see if the code works 
+		and does what it's supposed to do     */
+	FILE* noisy_wave_file; //this is my input file
+	noisy_wave_file = fopen("noisy_sin_wave.DAT","r");
+	//smooth the noisy wave
+	float temp_x;
+	float temp_y;
+	int counter=0; //to find number of elements in the noisy wave
 
-	/* Might not always know the length; you might have to run through
-	the data and find it sometimes*/
-	int len = 500;				
-	float arr[len];
-	float output[len-4];		// len - 4 because the moving average in the function loses 4 data points
-
-	FILE* noisySineWave;		// this is the noisy wave I created that I want to read off of
-
-	float x[len];				// need two separate arrays
-	float y[len];				// one each for x, y values
-
-	noisySineWave = fopen("noisySineWave.dat","r");		// read only file
-
-	if (noisySineWave != NULL){							// this turns the input data into an array
-		for (int i = 0; i < len; i++){	
-			fscanf(noisySineWave,"%f %f",&x[i],&y[i]);
+	if(noisy_wave_file != NULL){ //this is reading all the lines in the file to determine the size
+		while(fscanf(noisy_wave_file,"%f %f",&temp_x,&temp_y)!= EOF){
+			counter++;
 		}
+
+	float noisy_wave[counter]; //make an array that holds all the y values
+	rewind(noisy_wave_file);
+
+		for(int i = 0; i < counter; i++){ // this is reading the lines and placing them into x and y values
+			fscanf(noisy_wave_file,"%f %f", &temp_x, &noisy_wave[i]); //do I need &?
+		}
+
+
+		
+	float smoothed_wave[counter]; //this holds the y values of the wave after filtering
+	smoothCurve(noisy_wave, smoothed_wave, counter); //run the smoothing function
+
+
+	//output my smoothed wave to a file so I can plot it
+	FILE* smoothed_output_file;
+	smoothed_output_file = fopen("Smoothed_data.DAT","w");
+	for(int j = 0; j < counter; j++){
+		fprintf(smoothed_output_file,"\n%f",smoothed_wave[j]);
 	}
-	else printf("\nnoisySineWave didn't open");
 
-	smoothCurve(len,y,output); 					// calls function where it smooths the curve
-	float amp = amplitude(len,y);					// this function finds the amplitude
+	fclose(noisy_wave_file);
+	fclose(smoothed_output_file);
+	//now to use the amplitude function
+	float amp = 0;
+	amp = amplitude(smoothed_wave, counter);
+	printf("\nAmplitude is: %f",amp);
+	}
+	else printf("\nCould not find file");
 
-	printf("\nAmplitude is %f",amp);
 
-	/* Print out the new, smoothed data to a file */
-
-	return 0;
+	
+	
 }
--- task4/src/smoothCurve.c
+++ task4/src/smoothCurve.c
@@ -7,13 +7,13 @@
 	- done by averaging data */
 
 void smoothCurve(int len, float* input, float* output){	
-	float avg;				
+	float avg;			
 
 	for(int i = 0; i < len; i++){ 			// this loop cycles through all the data							
-		avg = 0;							// reset every time			
+		avg = 0;	
 
-		for(int j = 0; j < 5; j++){			// this loop averages the points
-			avg += input[j]/5;
+		for(int j = 0; j < 5; j++){			// this loop uses a moving average to average the data
+			avg = (float)input[j]/5;				// is it supposed to be input[i+j]? Why?
 		}
 
 		output[i] = avg;					// fill new array
